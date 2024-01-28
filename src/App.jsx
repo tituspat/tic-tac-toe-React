e59fdd652ad6a346b2ac7726efe8a1e81a98663f
drkpega <tituspat0112@gmail.com>
2024-01-28 23:18:25 +0700

    feat: tic-tac-toe

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,17 @@ export default function Board() {
     nextSquares[i] = xIsNext ? 'X' : 'O';
 
     setSquares(nextSquares);
+    console.log(nextSquares);
     setXIsNext(!xIsNext);
   }
 
   const winner = calculateWinner(squares);
   let status = '';
-  status = winner ? 'Winner: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O');
+  if (winner == 'Tie') {
+    status = 'It is a ' + winner;
+  } else {
+    status = winner ? 'Winner: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O');
+  }
 
   return (
     <>
@@ -59,9 +64,12 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
 
-    if (squares[a] && squares[a] === squares[c] && squares[b]) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
-    return false;
   }
+  if (squares.every((square) => square !== null)) {
+    return 'Tie';
+  }
+  return false;
 }
